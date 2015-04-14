bfed000c918b287b9ef48069c068eed0f5e9d308
Aleksey Uimanov <s9gf4ult@gmail.com>
2015-04-14 20:41:27 +0500

    reimplementing hotp/totp with cryptohash

--- Data/OTP.hs
+++ Data/OTP.hs
@@ -1,39 +1,55 @@
 -- |Implements HMAC-Based One-Time Password Algorithm as defined in RFC 4226 and
 -- Time-Based One-Time Password Algorithm as defined in RFC 6238.
-module Data.OTP (hotp, totp) where
+module Data.OTP
+       ( hotp
+       , totp
+       ) where
 
-import Codec.Utils
+import Crypto.Hash
 import Data.Bits
-import Data.HMAC
+import Data.Byteable
+import Data.ByteString       (ByteString)
+import Data.Serialize.Get
+import Data.Serialize.Put
 import Data.Time.Clock
 import Data.Time.Clock.POSIX
+import Data.Word
+
+import qualified Data.ByteString as BS
 
 -- | Compute an HOTP using secret key and counter value.
-hotp
-    :: [Octet]  -- ^ Secret key
-    -> Int      -- ^ Counter value
-    -> Int      -- ^ Number of digits in password
-    -> Int      -- ^ HOTP
-hotp key count digit' = truncate_hotp (hmac_sha1 key count') digit'
-    where truncate_hotp hmac_result digit' = snum `mod` (10 ^ digit')
-            where snum  = fromTwosComp sbits
-                  sbits = dt hmac_result
-                  dt hmac_r = [(head p) .&. 0x7F] ++ (tail p)
-                      where offsetBits = (hmacr !! 19) .&. 0xF
-                            offset = fromTwosComp [offsetBits]
-                            p = take 4 (drop offset hmacr)
-                            hmacr = pad hmac_r
-                                where pad xs = if length xs < 20 then pad (0 : xs) else xs
-          count' = pad (toTwosComp count)
-              where pad xs = if length xs < 8 then pad (0 : xs) else xs
+hotp :: (HashAlgorithm a)
+     => a                       -- ^ Hashing algorithm from module "Crypto.Hash"
+     -> ByteString              -- ^ Shared secret
+     -> Word64                  -- ^ Counter value
+     -> Word                    -- ^ Number of digits in password
+     -> Word32                  -- ^ HOTP
+hotp alg secr cnt digit =
+    let h = truncate
+            $ toBytes
+            $ hmacAlg alg secr
+            $ runPut
+            $ putWord64be cnt
+    in h `mod` (10^digit)
+  where
+    truncate :: ByteString -> Word32
+    truncate b =
+        let offset = BS.last b .&. 15
+            rb = BS.take 4 $ BS.drop (fromIntegral offset) b
+        in case runGet getWord32le rb of
+            Left e -> error e
+            Right b -> shiftR b 1 -- get last 31 bits as a number
+
 
 -- | Compute an TOTP using secret key and time.
-totp
-    :: [Octet]  -- ^ Secret key
-    -> UTCTime  -- ^ Time
-    -> Int      -- ^ Number of digits in password
-    -> Int      -- ^ Period
-    -> Int      -- ^ TOTP
-totp key time digit' period = hotp key timeCounter digit'
-    where timePOSIX = utcTimeToPOSIXSeconds time
-          timeCounter = (floor timePOSIX) `div` period
+totp :: (HashAlgorithm a)
+     => a                       -- ^ Hash algorithm to use
+     -> ByteString              -- ^ Shared secret
+     -> UTCTime                 -- ^ Time of TOTP
+     -> Word64                  -- ^ Time period
+     -> Word                    -- ^ Number of digits in password
+     -> Word32                  -- ^ TOTP
+totp alg secr time period digits =
+    let timePOSIX = floor $ utcTimeToPOSIXSeconds time
+        timeCounter = timePOSIX `div` period
+    in hotp alg secr timeCounter digits
