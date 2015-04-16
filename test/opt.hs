1d73eba8520cfb1687093df0df36e813d02c3d04
Aleksey Uimanov <s9gf4ult@gmail.com>
2015-04-16 16:38:19 +0500

    making tests work

--- test/opt.hs
+++ test/opt.hs
@@ -1,5 +1,6 @@
 module Main where
 
+import Control.Arrow
 import Crypto.Hash
 import Data.ByteString  (ByteString)
 import Data.OTP
@@ -28,12 +29,60 @@ hotpResults =
     ]
 
 
--- totp_t = zipWith (\x y -> (totp secret_otp x 8 30) == y) (map (read) ["1970-01-01 00:00:59 UTC",
---         "2005-03-18 01:58:29 UTC", "2005-03-18 01:58:31 UTC", "2009-02-13 23:31:30 UTC",
---         "2033-05-18 03:33:20 UTC"])
---         [94287082, 07081804, 14050471, 89005924, 69279037]
+data SomeAlg = forall a. (HashAlgorithm a, Show a) => SomeAlg { getAlg :: a }
+
+instance Show SomeAlg where
+    show (SomeAlg a) = show a
+
+
+testTotp :: (UTCTime, SomeAlg, Word32) -> TestTree
+testTotp (key, alg', result) =
+    testCase (show alg' ++ " => " ++ show result)
+    $ case alg' of
+          SomeAlg alg -> do
+              let t = totp alg hotpSecret key 30 8
+              result @=? t
+
+totpData :: [(UTCTime, SomeAlg, Word32)]
+totpData =
+    [ (read "1970-01-01 00:00:59 UTC", SomeAlg SHA1, 94287082)
+    , (read "1970-01-01 00:00:59 UTC", SomeAlg SHA256, 46119246)
+    , (read "1970-01-01 00:00:59 UTC", SomeAlg SHA512, 90693936)
+    ]
+
+  -- |  1111111109 |  2005-03-18  | 00000000023523EC | 07081804 |  SHA1  |
+  -- |             |   01:58:29   |                  |          |        |
+  -- |  1111111109 |  2005-03-18  | 00000000023523EC | 68084774 | SHA256 |
+  -- |             |   01:58:29   |                  |          |        |
+  -- |  1111111109 |  2005-03-18  | 00000000023523EC | 25091201 | SHA512 |
+  -- |             |   01:58:29   |                  |          |        |
+  -- |  1111111111 |  2005-03-18  | 00000000023523ED | 14050471 |  SHA1  |
+  -- |             |   01:58:31   |                  |          |        |
+  -- |  1111111111 |  2005-03-18  | 00000000023523ED | 67062674 | SHA256 |
+  -- |             |   01:58:31   |                  |          |        |
+  -- |  1111111111 |  2005-03-18  | 00000000023523ED | 99943326 | SHA512 |
+  -- |             |   01:58:31   |                  |          |        |
+  -- |  1234567890 |  2009-02-13  | 000000000273EF07 | 89005924 |  SHA1  |
+  -- |             |   23:31:30   |                  |          |        |
+  -- |  1234567890 |  2009-02-13  | 000000000273EF07 | 91819424 | SHA256 |
+  -- |             |   23:31:30   |                  |          |        |
+  -- |  1234567890 |  2009-02-13  | 000000000273EF07 | 93441116 | SHA512 |
+  -- |             |   23:31:30   |                  |          |        |
+  -- |  2000000000 |  2033-05-18  | 0000000003F940AA | 69279037 |  SHA1  |
+  -- |             |   03:33:20   |                  |          |        |
+  -- |  2000000000 |  2033-05-18  | 0000000003F940AA | 90698825 | SHA256 |
+  -- |             |   03:33:20   |                  |          |        |
+  -- |  2000000000 |  2033-05-18  | 0000000003F940AA | 38618901 | SHA512 |
+  -- |             |   03:33:20   |                  |          |        |
+  -- | 20000000000 |  2603-10-11  | 0000000027BC86AA | 65353130 |  SHA1  |
+  -- |             |   11:33:20   |                  |          |        |
+  -- | 20000000000 |  2603-10-11  | 0000000027BC86AA | 77737706 | SHA256 |
+  -- |             |   11:33:20   |                  |          |        |
+  -- | 20000000000 |  2603-10-11  | 0000000027BC86AA | 47863826 | SHA512 |
 
 
 main :: IO ()
 main = defaultMain $ testGroup "unit tests"
-    [ testGroup "hotp" $ map (uncurry testHotp) $ zip [0..] hotpResults ]
+    [ testGroup "hotp" $ map (uncurry testHotp) $ zip [0..] hotpResults
+    , testGroup "totp" $ map testTotp totpData
+    ]
